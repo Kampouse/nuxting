c75a02f46bf30050bae02baaf93ce9db51598432
jean-philippe <jp@zelexio.com>
2024-02-18 18:55:01 -0500

    add fe that is barebone nuxt

---
+++ front-end/pages/hello.vue
@@ -0,0 +1,30 @@
+
+
+
+
+
+
+<template>
+    <main>
+        <input ref="fileInput" type="file" @change="handleFileChange" />
+        <button @click="doSomething">do something</button>
+    </main>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const fileInput = ref<HTMLInputElement | null>(null)
+const files = ref()
+
+function handleFileChange() {
+    files.value = fileInput.value?.files
+}
+
+function doSomething() {
+    const file = files.value[0]
+    console.log(file)
+    // and do other things...
+}
+</script>
+
