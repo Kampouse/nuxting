0a2b89a967583cfaa21b92d82b9ff69600d5882f
jean-philippe <jp@zelexio.com>
2024-03-07 16:12:57 -0500

    did it work

--- app/parser/seed.ts
+++ app/parser/seed.ts
@@ -1,19 +1,39 @@
 import {MockData } from  "./index"
 import { useDB } from '~/server/db/drizzle';
-import { userTable  } from '~/server/db/schema';
+import * as tables from '~/server/db/schema';
 
 
 
 
- const  main = () => {
+const main = () => {
 
 
-const db = useDB()
+    const db = useDB()
 
-MockData()
-db.select().from(userTable).execute().then((result) => {
-    console.log(result);
-});
+    const data = MockData().then((data) => {
+        let sharedid = 0
+        data?.transformed.forEach(async (element) => {
+
+
+
+
+            const order_data = await db.insert(tables.orderInfoTable).values({
+                testOrdered: element.orderInfo.testOrdered,
+                observationDateTime: element.orderInfo.observationDateTime,
+                orderStatus: element.orderInfo.orderStatus
+            }).returning().execute()
+            if (order_data) {
+                order_data[0].id
+                const patient_data = db.insert(tables.userTable).values({
+                    patientID: element.patientInfo.patientID,
+                    name: element.patientInfo.name,
+                    dob: element.patientInfo.dob,
+                    orderInfoTableId: order_data[0].id
+                }).returning().execute()
+            }
+
+        })
+    })
 
 
 
--- app/server/db/seed.ts
+++
@@ -1,7 +0,0 @@
-import { useDB } from "./drizzle";
-import type { InsertUser } from "./schema";
-import  { MockData }       from "~/parser/index"
-import { userTable } from "./schema";
-
- 
-MockData()
