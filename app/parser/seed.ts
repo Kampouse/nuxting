895811b8c709f2a43cd6b276bcd6ee77f7e49ce8
jean-philippe martel <j-p@jean-philippes-MacBook-Air.local>
2024-03-10 16:22:33 -0400

    added the csv into a  database:

--- app/parser/seed.ts
+++ app/parser/seed.ts
@@ -1,3 +1,4 @@
+import { eq } from 'drizzle-orm';
 import { MockData } from "./index"
 import { useDB } from '~/server/db/drizzle';
 import * as tables from '~/server/db/schema';
@@ -63,10 +64,63 @@ const main = () => {
         })
     }
 
+    const injectCSVdata = (input: Mockdata) => {
+
+
+        input.then((data) => {
+            data?.database_data.diag_metric.forEach(async (element) => {
+                db.insert(tables.diagnosticMetricTable).values({
+                    name: element.name,
+                    oruSonicCodes: element.oru_sonic_codes,
+                    diagnostic: element.diagnostic,
+                    diagnosticGroups: element.diagnostic_groups,
+                    oruSonicUnits: element.oru_sonic_units,
+                    units: element.units,
+                    minAge: element.min_age,
+                    maxAge: element.max_age,
+                    standardLower: element.standard_lower,
+                    standardHigher: element.standard_higher,
+                    everlabLower: element.everlab_lower,
+                    everlabHigher: element.everlab_higher,
+                    gender: element.gender
+                }).execute()
+            })
+            data?.database_data.diag.forEach(async (element) => {
+                db.insert(tables.diagnosticTable).values({
+                    name: element.name,
+                    diagnosticGroups: element.diagnostic_groups,
+                    diagnosticMetrics: element.diagnostic_metrics
+                }).execute()
+            })
+            data?.database_data.condition.forEach(async (element) => {
+                db.insert(tables.conditionTable).values({
+                    name: element.name,
+                    diagnostic: element.diagnostic ?? ""
+                }).execute()
+            })
+
+
+
+
+
+        })
+
+
+
+
+
+
+
+
+
+
+
+
+    }
     const data = MockData();
     injectBaseHL7Data(data)
-    console.log("now inserting csv data into the database...")
-     
+    injectCSVdata(data)
+
 }
 
 
--- app/server/db/schema.ts
+++ app/server/db/schema.ts
@@ -29,6 +29,50 @@ export const results = sqliteTable('results', {
   resultStatus: text('result_status'),
   orderInfoId: integer('order_info_id').references(() => orderInfoTable.id),
 });
+
+export const diagnosticMetricTable = sqliteTable('diagnostic_metric', {
+  name: text('name').notNull(),
+  oruSonicCodes: text('oru_sonic_codes').notNull(),
+  diagnostic: text('diagnostic').notNull(),
+  diagnosticGroups: text('diagnostic_groups').notNull(),
+  oruSonicUnits: text('oru_sonic_units').notNull(),
+  units: text('units').notNull(),
+  minAge: text('min_age').notNull(),
+  maxAge: text('max_age').notNull(),
+  standardLower: text('standard_lower').notNull(),
+  standardHigher: text('standard_higher').notNull(),
+  everlabLower: text('everlab_lower').notNull(),
+  everlabHigher: text('everlab_higher').notNull(),
+  gender: text('gender').notNull()
+});
+
+export const diagnosticTable = sqliteTable('diagnostic', {
+  name: text('name').notNull(),
+  diagnosticGroups: text('diagnostic_groups').notNull(),
+  diagnosticMetrics: text('diagnostic_metrics').notNull()
+});
+
+export const conditionTable = sqliteTable('condition', {
+  name: text('name').notNull(),
+  diagnostic: text('diagnostic').notNull()
+});
+
+export type InsertCondition = typeof conditionTable.$inferInsert
+export type SelectCondition = typeof conditionTable.$inferSelect
+
+export type InsertDiagnostic = typeof diagnosticTable.$inferInsert
+export type SelectDiagnostic = typeof diagnosticTable.$inferSelect
+
+export type InsertDiagnosticMetric = typeof diagnosticMetricTable.$inferInsert
+export type SelectDiagnosticMetric = typeof diagnosticMetricTable.$inferSelect
+
+
+
+
+
+
+
+
 export type InsertUser = typeof userTable.$inferInsert;
 export type SelectUser = typeof userTable.$inferSelect;
 export type InsertResults = typeof results.$inferInsert;
