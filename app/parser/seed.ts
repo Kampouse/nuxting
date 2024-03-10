0450b0c9857c24dd9db6d7611cb9ad488f43354e
jean-philippe <jp@zelexio.com>
2024-03-10 15:00:14 -0400

    first step for csv in the database

--- app/parser/seed.ts
+++ app/parser/seed.ts
@@ -3,53 +3,74 @@ import { useDB } from '~/server/db/drizzle';
 import * as tables from '~/server/db/schema';
 
 const main = () => {
-    const db = useDB()
+    let db = useDB()
 
-    const ouput = db.select().from(tables.userTable).execute().then((data) => {
-        if (data.length > 0) {
-            console.log("Data already is seeded. Exiting...")
-            process.exit(0)
+
+    type Mockdata = ReturnType<typeof MockData>
+
+
+
+
+
+    const injectBaseHL7Data = async (InputData: Mockdata) => {
+        const dataExists = await checkIfDataExists()
+        if (dataExists) {
+            console.log("Data already exists in the database. Exiting...")
+            return
         }
-        return data
-    })
-    const data = MockData().then((data) => {
-        let sharedid = 0
-        data?.transformed.forEach(async (element) => {
-            const order_data = await db.insert(tables.orderInfoTable).values({
-                testOrdered: element.orderInfo.testOrdered,
-                observationDateTime: element.orderInfo.observationDateTime,
-                orderStatus: element.orderInfo.orderStatus
-            }).returning().execute()
-            if (order_data) {
-                order_data[0].id
-                const patient_data = db.insert(tables.userTable).values({
-                    patientID: element.patientInfo.patientID,
-                    name: element.patientInfo.name,
-                    dob: element.patientInfo.dob,
-                    orderInfoTableId: order_data[0].id
+        InputData.then((data) => {
+            data?.transformed.forEach(async (element) => {
+                const order_data = await db.insert(tables.orderInfoTable).values({
+                    testOrdered: element.orderInfo.testOrdered,
+                    observationDateTime: element.orderInfo.observationDateTime,
+                    orderStatus: element.orderInfo.orderStatus
                 }).returning().execute()
-                element.orderInfo.results.forEach(async (result) => {
-                    const result_data = await db.insert(tables.results).values({
-                        test: result.test,
-                        value: result.value,
-                        units: result.units,
-                        referenceRange: result.referenceRange,
-                        resultStatus: result.resultStatus,
-                        orderInfoId: order_data[0].id
+                if (order_data) {
+                    order_data[0].id
+                    const patient_data = db.insert(tables.userTable).values({
+                        patientID: element.patientInfo.patientID,
+                        name: element.patientInfo.name,
+                        dob: element.patientInfo.dob,
+                        orderInfoTableId: order_data[0].id
                     }).returning().execute()
-                })
-            }
+                    element.orderInfo.results.forEach(async (result) => {
+                        const result_data = await db.insert(tables.results).values({
+                            test: result.test,
+                            value: result.value,
+                            units: result.units,
+                            referenceRange: result.referenceRange,
+                            resultStatus: result.resultStatus,
+                            orderInfoId: order_data[0].id
+                        }).returning().execute()
+                    })
+                }
+            })
 
         })
 
+    }
 
+    const checkIfDataExists = async () => {
 
-    })
+        if (!db) {
+            db = useDB()
+        }
+        return db.select().from(tables.userTable).execute().then((data) => {
+            if (data.length > 0) {
+                return true
+            }
+            return false
+        })
+    }
+
+    const data = MockData();
+    injectBaseHL7Data(data)
+    console.log("now inserting csv data into the database...")
+     
 }
 
 
 main()
-console.log("Data seeded successfully")
 
 
 
