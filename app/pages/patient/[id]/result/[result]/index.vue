e9c4ab6ade92edb6983947347a09eb74f3474d80
jean-philippe <jp@zelexio.com>
2024-03-12 12:11:16 -0400

    added better range thing

--- app/pages/patient/[id]/result/[result]/index.vue
+++ app/pages/patient/[id]/result/[result]/index.vue
@@ -25,38 +25,23 @@ const  filterData = computed(() => {
     return output
   }
 })
+
 const get_range = (value: string, lower: number, higher: number, range: number) => {
-
-function scaleBetween(unscaledNum:number, minAllowed:number, maxAllowed:number, min:number, max:number) {
-      return (maxAllowed - minAllowed) * (unscaledNum - min) / (max - min) + minAllowed;
+function categorizeValue(value: number, rangeStart: number, rangeEnd: number, thresholdPercentage: number): string {
+    // Normalize the value to a 0-1 scale
+    const normalizedValue = (value - rangeStart) / (rangeEnd - rangeStart);
+    // Convert thresholdPercentage into a decimal
+    const threshold = thresholdPercentage / 100;
+    if (normalizedValue <= threshold) {
+        return 'low';
+    } else if (normalizedValue >= 1 - threshold) {
+        return 'high';
+    } else {
+        return 'normal';
     }
-
-
-
-
+}
   const input = parseInt(value)
-
-    
-    const output =  scaleBetween(input, 0, 100, 0, 100)
-    const rangedLow = scaleBetween(lower, 0, 100, 0, 100)
-    const rangedAverage = scaleBetween((lower + higher) / 2, 0, 100, 0, 100)
-    const rangedHigh = scaleBetween(higher, 0, 100, 0, 100)
-     
-  console.log("=>",rangedLow,  rangedLow, rangedAverage, rangedHigh, "<=")
-   //check what is the closest to 0
-   if( output  <= rangedLow * 1.05 ) {
-     return "Low"
-   }
-   if( output * 1.05 < rangedAverage ) {
-     return "average"
-   }
-    if( output <= rangedHigh) {
-      return "High"
-    }
-    console.log("=>",output, rangedLow, rangedAverage, rangedHigh, "<=")
-
-
-   
+   return categorizeValue(input, lower, higher, range) 
 }
 
 const  filterResult = computed(() => {
@@ -64,16 +49,11 @@ const  filterResult = computed(() => {
     return data.value?.output
   }
 })
-
-
-
 </script>
 <template>
       <div>
-    <div class="    flex  items-center justify-left pl-2    font-mono  from-neutral-950 text-3xl    h-12 bg-[#61A3BC]">
-      
+    <div class="flex items-center justify-left pl-2 font-mono from-neutral-950 text-3xl h-12 bg-[#61A3BC]">
      <NuxtLink :to="`/`">patient=> </NuxtLink>result</div>
-            
 <Table>
     <TableHeader>
       <TableRow>
@@ -87,38 +67,29 @@ const  filterResult = computed(() => {
         <TableHead class="text-center"> Reference </TableHead>
       </TableRow>
     </TableHeader>
-
 <div v-if="pending"></div>
     <TableBody v-else>
       <TableRow class=""  v-if="data" v-for="test in  filterData">
         <TableCell class="font-medium" >
           {{ test?.results.id }}
         </TableCell>
-          <TableCell 
-          > {{ test?.results.test }}
+          <TableCell >
+             {{ test?.results.test }}
             </TableCell>
-        <TableCell    class=" text-left">
+        <TableCell class=" text-left">
           {{ test?.results.value}}
-
         </TableCell>
          <TableCell class="text-center">
           {{ test?.results.referenceRange}}
-
         </TableCell>
           <TableCell class="text-center">
-            {{ get_range(test?.results.value, test?.diagnostic_metric.standardLower, test?.diagnostic_metric.standardHigher, 0.35) }}
-
+            {{ get_range(test?.results.value, test?.diagnostic_metric.standardLower, test?.diagnostic_metric.standardHigher, 30) }}
         </TableCell>
           <TableCell class="text-center">
           {{ test?.diagnostic_metric.standardLower }}-{{ test?.diagnostic_metric.standardHigher}}
-
         </TableCell>
-
-
-
       </TableRow>
     </TableBody>
   </Table>
       </div>      
-      
 </template>
